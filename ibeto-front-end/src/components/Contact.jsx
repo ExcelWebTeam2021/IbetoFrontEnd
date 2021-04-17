66cc7c0ab00c90d6956b07fac1c68e25ca3c2e96
annu12340 <annujolly17@gmail.com>
2021-04-17 17:34:02 +0530

    added the layout for the site

---
+++ ibeto-front-end/src/components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Style.css";
+import "./ResponsiveStyle.css";
+
+const Contact = () => {
+   return (
+      <div>
+      
+      </div>
+   );
+};
+
+export default Contact;
---
+++ ibeto-front-end/src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Style.css";
+
+const Navbar = () => {
+   return <div className='navbar'></div>;
+};
+
+export default Navbar;
