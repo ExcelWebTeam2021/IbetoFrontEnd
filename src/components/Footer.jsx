93263e9568790f0486b7730de5225a0c6571bbf9
annu12340 <annujolly17@gmail.com>
2021-04-17 20:30:33 +0530

    njan maduthu

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Style.css";
-import "./ResponsiveStyle.css";
-
-const Footer = () => {
-   return <div>footer</div>;
-};
-
-export default Footer;
--- src/components/Navbar.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Style.css";
-
-const Navbar = () => {
-   return <div className='navbar'></div>;
-};
-
-export default Navbar;
