61d8fd0c800e722961c8f8d7497110a1b2455c3c
annu12340 <annujolly17@gmail.com>
2021-05-05 12:52:09 +0530

    trial commit

--- ibeto-front-end/src/components/ibetoMainSite/About.jsx
+++ ibeto-front-end/src/components/ibetoMainSite/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
    return (
       <div class='container-fluid lighter-bk'>
          <div className='row about'>
-            <h4 className='heading'>About</h4>
+            <h4 className='heading'>About ibeto</h4>
             <p className='content'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae provident commodi et vero voluptates reprehenderit repellat similique in maxime quibusdam.</p>
             <button class='btn btn-dark'>mmm</button>
          </div>
