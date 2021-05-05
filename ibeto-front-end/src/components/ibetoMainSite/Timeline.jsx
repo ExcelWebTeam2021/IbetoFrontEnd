bb612bd8271e484cab47c92dacfe949a5d078e41
aishh2k <aiswarya2000may@gmail.com>
2021-05-04 17:05:19 -0700

    Timeline fixed

--- ibeto-front-end/src/components/ibetoMainSite/Timeline.jsx
+++ ibeto-front-end/src/components/ibetoMainSite/Timeline.jsx
@@ -4,43 +4,43 @@ import "../stylesheet/Timeline.css";
 
 const Timeline = () => {
     return (
-            <div className="container center">
-                <h3 className="card-title">Timeline Style</h3>
+            <div className="container mb-5">
+                <h3 className="heading timeline-head3">Timeline</h3>
                 <div className="row">
                     <div className="col-md-12">
                         <div className="main-timeline3">
-                            <div className="timeline">
-                                <div className="timeline-icon"><span className="year">2018</span></div>
-                                <div className="timeline-content">
-                                    <h3 className="title">Web Desginer</h3>
-                                    <p className="description">
+                            <div className="timeline3">
+                                <div className="timeline-icon3"><span className="year3">2018</span></div>
+                                <div className="timeline-content3">
+                                    <h3 className="title3">Web Desginer</h3>
+                                    <p className="description3">
                                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia mi ultrices, luctus nunc ut, commodo enim. Vivamus sem erat.
                                     </p>
                                 </div>
                             </div>
-                            <div className="timeline">
-                                <div className="timeline-icon"><span className="year">2017</span></div>
-                                <div className="timeline-content">
-                                    <h3 className="title">Web Developer</h3>
-                                    <p className="description">
+                            <div className="timeline3">
+                                <div className="timeline-icon3"><span className="year3">2017</span></div>
+                                <div className="timeline-content3">
+                                    <h3 className="title3">Web Developer</h3>
+                                    <p className="description3">
                                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia mi ultrices, luctus nunc ut, commodo enim. Vivamus sem erat.
                                     </p>
                                 </div>
                             </div>
-                            <div className="timeline">
-                                <div className="timeline-icon"><span className="year">2016</span></div>
-                                <div className="timeline-content">
-                                    <h3 className="title">Web Desginer</h3>
-                                    <p className="description">
+                            <div className="timeline3">
+                                <div className="timeline-icon3"><span className="year3">2016</span></div>
+                                <div className="timeline-content3">
+                                    <h3 className="title3">Web Desginer</h3>
+                                    <p className="description3">
                                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia mi ultrices, luctus nunc ut, commodo enim. Vivamus sem erat.
                                     </p>
                                 </div>
                             </div>
-                            <div className="timeline">
-                                <div className="timeline-icon"><span className="year">2015</span></div>
-                                <div className="timeline-content">
-                                    <h3 className="title">Web Developer</h3>
-                                    <p className="description">
+                            <div className="timeline3">
+                                <div className="timeline-icon3"><span className="year3">2015</span></div>
+                                <div className="timeline-content3">
+                                    <h3 className="title3">Web Developer</h3>
+                                    <p className="description3">
                                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia mi ultrices, luctus nunc ut, commodo enim. Vivamus sem erat.
                                     </p>
                                 </div>
