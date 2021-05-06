4da6d813ca49afb8dfafb3b5a8b4978f53b0d415
annu12340 <annujolly17@gmail.com>
2021-05-06 23:32:13 +0530

    Adding the audio

--- ibeto-front-end/src/components/ibetoMainSite/LandingPage.jsx
+++ ibeto-front-end/src/components/ibetoMainSite/LandingPage.jsx
@@ -1,14 +1,19 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import lottie from "lottie-web";
 
 import "../stylesheet/LandingPage.css";
 import "../stylesheet/MainSiteStyle.css";
 import "../stylesheet/ResponsiveStyle.css";
 
+import audio from "./audio.mp3";
+
 const LandingPage = () => {
    const LandingPage = useRef(null);
+   const audioRef = useRef(new Audio(audio));
+   var [toggle, setToggle] = useState(true);
 
    useEffect(() => {
+      audioRef.current.play();
       const anime1 = lottie.loadAnimation({
          container: LandingPage.current,
          renderer: "svg",
@@ -19,12 +24,19 @@ const LandingPage = () => {
 
       return () => {
          anime1.destroy();
-      }; // optional clean up for unmounting
+      }; // clean up for unmounting
    }, []);
+
+   function handlePause() {
+      setToggle(!toggle);
+      console.log("Button is toggled", toggle);
+   }
 
    return (
       <main>
          <div className='animation-container' ref={LandingPage} />
+         <audio ref={audioRef} src={audio} muted={!toggle} />
+         <button onClick={handlePause}> {toggle ? <img src='https://image.flaticon.com/icons/png/128/727/727269.png' alt='unmute' width='20' /> : <img src='https://image.flaticon.com/icons/png/128/56/56882.png' alt='mute' width='20' />}</button>
       </main>
    );
 };
