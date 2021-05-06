1113f4ee0b66a1fe6e55a3da6589ea434972f7e5
gayathri-venu <gayathrivenu200@gmail.com>
2021-05-06 20:21:56 +0530

    Change Contact

--- ibeto-front-end/src/components/ibetoMainSite/Contact.jsx
+++ ibeto-front-end/src/components/ibetoMainSite/Contact.jsx
@@ -10,19 +10,18 @@ const Contact = () => {
      <img src="https://i.imgur.com/bZBG9PE.jpg" alt="image1" class="profile-icon" />
      <div class="profile-name">Kelly Seikma</div>
      <div class="profile-position">Web Designer</div>
-     <a href="#" class="button">Connect</a>
    </div>
    <div class="profile-card">
      <img src="https://i.imgur.com/S4GNFIW.jpg" alt="image2" class="profile-icon" />
      <div class="profile-name">Mabel Maxwell</div>
      <div class="profile-position">Web Developer</div>
-     <a href="#" class="button">Connect</a>
+     
    </div>
    <div class="profile-card">
      <img src="https://i.imgur.com/cMSVQZC.jpg" alt="image3" class="profile-icon" />
      <div class="profile-name">Danny Liswell</div>
      <div class="profile-position">DevOps</div>
-     <a href="#" class="button">Connect</a>
+    
    </div>
  </div>
  );
