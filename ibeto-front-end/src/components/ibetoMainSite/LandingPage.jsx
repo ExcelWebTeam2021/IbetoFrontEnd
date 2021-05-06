235beec8db8ad816d67e704ee058ad41294e0ac7
annu12340 <annujolly17@gmail.com>
2021-05-07 00:49:28 +0530

    again fixing bug in audio- attempt4

--- ibeto-front-end/src/components/ibetoMainSite/LandingPage.jsx
+++ ibeto-front-end/src/components/ibetoMainSite/LandingPage.jsx
@@ -10,14 +10,30 @@ import audio from "./audio.mp3";
 const LandingPage = () => {
    const LandingPage = useRef(null);
    const audioRef = useRef(new Audio(audio));
+   const [Control, setControl] = useState(false);
+   const [Autoplay, setAutoplay] = useState(true);
 
    useEffect(() => {
-      audioRef.current.play();
+      var promise = audioRef.current.play();
+      if (promise !== undefined) {
+         promise
+            .then((_) => {
+               // !
+               console.log("Autoplay started ");
+            })
+            .catch((error) => {
+               console.log("error in audio ");
+               setControl(true);
+               setAutoplay(false);
+               // Autoplay was prevented.
+               // Show a "Play" button so that user can start playback.
+            });
+      }
       const anime1 = lottie.loadAnimation({
          container: LandingPage.current,
          renderer: "svg",
          loop: true,
-         autoplay: true,
+         autoplay: { Autoplay },
          animationData: require("./animations/LandingPageAnimation.json"),
       });
 
@@ -29,7 +45,7 @@ const LandingPage = () => {
    return (
       <main>
          <div className='animation-container' ref={LandingPage} />
-         <audio ref={audioRef} src={audio} muted={false} autoplay controls />
+         <audio src={audio} muted={false} autoPlay controls={Control} />
       </main>
    );
 };
