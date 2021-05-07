6feeddbd9bca9b072520890b9217dcbd74939342
annu12340 <annujolly17@gmail.com>
2021-05-07 21:51:00 +0530

    Fixed the bug in testimonial section

--- ibeto-front-end/src/components/ibetoMainSite/Contact.jsx
+++ ibeto-front-end/src/components/ibetoMainSite/Contact.jsx
@@ -5,26 +5,27 @@ import "../stylesheet/ResponsiveStyle.css";
 
 const Contact = () => {
    return (
-   <div class="profile-container">
-   <div class="profile-card">
-     <img src="https://i.imgur.com/bZBG9PE.jpg" alt="image1" class="profile-icon" />
-     <div class="profile-name">Kelly Seikma</div>
-     <div class="profile-position">Web Designer</div>
-   </div>
-   <div class="profile-card">
-     <img src="https://i.imgur.com/S4GNFIW.jpg" alt="image2" class="profile-icon" />
-     <div class="profile-name">Mabel Maxwell</div>
-     <div class="profile-position">Web Developer</div>
-     
-   </div>
-   <div class="profile-card">
-     <img src="https://i.imgur.com/cMSVQZC.jpg" alt="image3" class="profile-icon" />
-     <div class="profile-name">Danny Liswell</div>
-     <div class="profile-position">DevOps</div>
-    
-   </div>
- </div>
- );
+      <div className='container'>
+         <h1 class='heading'>Contact Us</h1>
+         <div class='profile-container'>
+            <div class='profile-card'>
+               <img src='https://i.imgur.com/bZBG9PE.jpg' alt='image1' class='profile-icon' />
+               <div class='profile-name'>Kelly Seikma</div>
+               <div class='profile-position'>Web Designer</div>
+            </div>
+            <div class='profile-card'>
+               <img src='https://i.imgur.com/S4GNFIW.jpg' alt='image2' class='profile-icon' />
+               <div class='profile-name'>Mabel Maxwell</div>
+               <div class='profile-position'>Web Developer</div>
+            </div>
+            <div class='profile-card'>
+               <img src='https://i.imgur.com/cMSVQZC.jpg' alt='image3' class='profile-icon' />
+               <div class='profile-name'>Danny Liswell</div>
+               <div class='profile-position'>DevOps</div>
+            </div>
+         </div>
+      </div>
+   );
 };
 
 export default Contact;
--- ibeto-front-end/src/components/ibetoMainSite/Testimonials.jsx
+++ ibeto-front-end/src/components/ibetoMainSite/Testimonials.jsx
@@ -6,64 +6,78 @@ import "../stylesheet/Testimonials.css";
 
 const Testimonials = () => {
    return (
+      <div className='container testimonial-row'>
+         <h1 class='heading'>Testimonials</h1>
+         <div id='carouselExampleIndicators' class='carousel slide' data-ride='carousel'>
+            <ol class='carousel-indicators'>
+               <li data-target='#carouselExampleIndicators' data-slide-to='0' class='active'></li>
+               <li data-target='#carouselExampleIndicators' data-slide-to='1'></li>
+               <li data-target='#carouselExampleIndicators' data-slide-to='2'></li>
+            </ol>
+         </div>
 
-    <div className="container">
-        <div className="row">
-            <div className="col-md-12">
-                <div className="carousel slide" data-ride="carousel" id="quote-carousel">
-                    <div className="carousel-inner text-center">
-                       
-                        <div className="item active">
-                            <blockquote>
-                                <div className="row">
-                                    <div className="col-sm-8 col-sm-offset-2">
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. !</p>
-                                        <small>Someone famous</small>
-                                    </div>
-                                </div>
-                            </blockquote>
-                        </div>
+         <div className='row'>
+            <div className='col-md-12'>
+               <div className='carousel slide' data-ride='carousel' id='quote-carousel'>
+                  <div className='carousel-inner text-center'>
+                     <div className='item active carousel-item'>
+                        <blockquote>
+                           <div className='row'>
+                              <div className='col-sm-8 col-sm-offset-2'>
+                                 <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. !
+                                 </p>
+                                 <small>Someone famous</small>
+                              </div>
+                           </div>
+                        </blockquote>
+                     </div>
 
-                        <div className="item">
-                            <blockquote>
-                                <div className="row">
-                                    <div className="col-sm-8 col-sm-offset-2">
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
-                                        <small>Someone famous</small>
-                                    </div>
-                                </div>
-                            </blockquote>
-                        </div>
-                        
-                        <div className="item">
-                            <blockquote>
-                                <div className="row">
-                                    <div className="col-sm-8 col-sm-offset-2">
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. .</p>
-                                        <small>Someone famous</small>
-                                    </div>
-                                </div>
-                            </blockquote>
-                        </div>
-                    </div>
-                   
-                    <ol className="carousel-indicators">
-                        <li data-target="#quote-carousel" data-slide-to="0" className="active"><img className="img-responsive " src="https://s3.amazonaws.com/uifaces/faces/twitter/mantia/128.jpg" alt=""/>
-                        </li>
-                        <li data-target="#quote-carousel" data-slide-to="1"><img className="img-responsive" src="https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg" alt=""/>
-                        </li>
-                        <li data-target="#quote-carousel" data-slide-to="2"><img className="img-responsive" src="https://s3.amazonaws.com/uifaces/faces/twitter/brad_frost/128.jpg" alt=""/>
-                        </li>
-                    </ol>
+                     <div className='item carousel-item'>
+                        <blockquote>
+                           <div className='row'>
+                              <div className='col-sm-8 col-sm-offset-2'>
+                                 <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.{" "}
+                                 </p>
+                                 <small>Someone famous</small>
+                              </div>
+                           </div>
+                        </blockquote>
+                     </div>
 
-                    
-                    <a data-slide="prev" href="#quote-carousel" className="left carousel-control"><i className="fa fa-chevron-left"></i></a>
-                    <a data-slide="next" href="#quote-carousel" className="right carousel-control"><i className="fa fa-chevron-right"></i></a>
-                </div>
+                     <div className='item carousel-item'>
+                        <blockquote>
+                           <div className='row'>
+                              <div className='col-sm-8 col-sm-offset-2'>
+                                 <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. .
+                                 </p>
+                                 <small>Someone famous</small>
+                              </div>
+                           </div>
+                        </blockquote>
+                     </div>
+                  </div>
+
+                  <ol className='carousel-indicators'>
+                     <li data-target='#quote-carousel' data-slide-to='0' className='active'>
+                        <img className='img-responsive ' src='https://s3.amazonaws.com/uifaces/faces/twitter/brad_frost/128.jpg' alt='' />
+                     </li>
+                     <li data-target='#quote-carousel' data-slide-to='1'>
+                        <img className='img-responsive' src='https://images.unsplash.com/photo-1599842057874-37393e9342df?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGdpcmx8ZW58MHx8MHx8' alt='' />
+                     </li>
+                     <li data-target='#quote-carousel' data-slide-to='2'>
+                        <img className='img-responsive' src='https://images.unsplash.com/photo-1503185912284-5271ff81b9a8' alt='' />
+                     </li>
+                  </ol>
+               </div>
             </div>
-        </div>
-    </div>
-
+         </div>
+      </div>
    );
 };
 
