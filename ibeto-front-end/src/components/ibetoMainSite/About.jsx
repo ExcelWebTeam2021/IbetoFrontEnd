aa5da8444bb373cb83af45ee0c25607eaa4873fc
annu12340 <annujolly17@gmail.com>
2021-05-07 18:15:10 +0530

    made some UI update in about and event format

--- ibeto-front-end/src/components/ibetoMainSite/About.jsx
+++ ibeto-front-end/src/components/ibetoMainSite/About.jsx
@@ -7,14 +7,13 @@ const About = () => {
    return (
       <div class='container-fluid lighter-bk'>
          <div className='row about'>
-            <div className='col-md-6 col-md-12 '>
-               {/* <img src="../images/event-img1.jpg" class="about-img" /> */}
-               <div className='about-img'>IMG</div>
+            <div className='col-md-4 col-xs-12 '>
+               <img src='https://ibeto.excelmec.org/assets/nikhil-01.png' class='img-fluid' />
             </div>
-            <div className='col-md-6 col-md-12 '>
-               <h1 class='about-header'>About</h1>
+            <div className='col-md-6 col-xs-12 '>
+               <h1 class='heading'>About</h1>
                <p className='content'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae provident commodi et vero voluptates reprehenderit repellat similique in maxime quibusdam.</p>
-               <div className='about-button'>
+               <div className=''>
                   <button class='btn btn-dark'>Register Now</button>
                </div>
             </div>
--- ibeto-front-end/src/components/ibetoMainSite/EventFormat.jsx
+++ ibeto-front-end/src/components/ibetoMainSite/EventFormat.jsx
@@ -1,47 +1,38 @@
-import React from 'react'
+import React from "react";
 import "../stylesheet/CommonStyle.css";
 import "../stylesheet/MainSiteStyle.css";
 import "../stylesheet/ResponsiveStyle.css";
 
 export default function EventFormat() {
-    return (
-        <div className="container-fluid">
-            <div class="event-format">
-                <div className="event-header">
-                    <h1>Event Format</h1>
-                </div>
-                <div className="event-body">
-                    <div className="row">
-                        <div class="event-box col-lg-4">
-                            <img src="../images/event-img1" alt=""/>
-                            <span class="phase">Phase 1</span>
-                            <h3 class="event-title">First</h3>
-                            <p class="event-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        </div>
+   return (
+      <div className='container  event-format'>
+         <div className='heading'>
+            <h1>Event Format</h1>
+         </div>
+         <div className='event-body'>
+            <div className='row'>
+               <div class='col-md-4 col-sm-10 phase'>
+                  <img src='https://ibeto2019.netlify.app/img/ideate.png' width='200' alt='' />
+                  <span class=''>Phase 1</span>
+                  <h3 class='event-title'>First</h3>
+                  <p class='event-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+               </div>
 
-                        <div class="event-box col-lg-4">
-                            <span class="phase">Phase 2</span>
-                            <h3 class="event-title">Second</h3>
-                            <p class="event-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        </div>
+               <div class='col-md-4 col-sm-10 phase'>
+                  <img src='https://ibeto2019.netlify.app/img/ideate.png' width='200' alt='' />
+                  <span>Phase 2</span>
+                  <h3 class='event-title'>Second</h3>
+                  <p class='content'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+               </div>
 
-                        <div class="event-box col-lg-4">
-                            <span class="phase">Phase 3</span>
-                            <h3 class="event-title">Third</h3>
-                            <p class="event-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        </div>
-
-
-                    </div>
-
-
-
-
-
-                </div>
+               <div class='col-md-4 col-sm-10 phase'>
+                  <img src='https://ibeto2019.netlify.app/img/ideate.png' width='200' alt='' />
+                  <span>Phase 3</span>
+                  <h3 class='event-title'>Third</h3>
+                  <p class='event-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+               </div>
             </div>
-        </div>
-    )
+         </div>
+      </div>
+   );
 }
-
-
