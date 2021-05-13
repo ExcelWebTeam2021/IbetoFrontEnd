04b70e9457081eb5d3091761f726a5565a324b01
annu12340 <annujolly17@gmail.com>
2021-05-13 23:26:28 +0530

    Added the footer

--- ibeto-front-end/src/components/ibetoMainSite/Footer.jsx
+++ ibeto-front-end/src/components/ibetoMainSite/Footer.jsx
@@ -3,79 +3,40 @@ import "../stylesheet/MainSiteStyle.css";
 import "../stylesheet/ResponsiveStyle.css";
 
 const Footer = () => {
-    return <div className='footer'>
-        <footer class="section section-8 flex-rw">
-            <div className="footerDiv">
-                <div className="footerIbeto">
-                    <a href="http://www.mec.ac.in">
-                        <img src="../images/Copy of mec_logo.png" alt=""/>
-                    </a>
-                </div>
-                <div className="icon1">
-                    <a href="//twitter.com/excelmec?lang=en">
-                        <img src="../images/tweet.svg" alt=""/>
-                    </a>
-                </div>
-                <div className="icon2">
-                    <a href="//www.facebook.com/excelmec/">
-                        <img src="../images/fb.svg" alt=""/>
-                    </a>
-                </div>
-                 <div className="icon3">
-                    <a href="//www.instagram.com/excelmec/?hl=en">
-                        <img src="../images/instagram (2).png" alt=""/>
-                    </a>
-                </div>
-                <div className="footerExcel">
-                    <a href="//www.excelmec.org">
-                        <img src="../images/excel 2019.png" alt=""/>
-                    </a>
-                </div>
-                
-                <div className="Excelbrand">
-                          {/* <span>Made with <i class="far fa-heart" style="color:coral;"></i> Excel 2019</span> */}  
-                           {/* Font awesome heart not working */}
-                          <span>Made with  Excel 2019</span>
-                </div> 
+   return (
+      <div className='footer container-fluid'>
+         <div className='row footer-row'>
+            <div className='col-4 '>
+               <img src='https://ibeto.excelmec.org/assets/Copy%20of%20mec_logo.png' alt='' width='140' />
             </div>
-        </footer>
-    </div>;
-        // <div className='footer'>
-        //     <footer class="section section-8 flex-rw">
-        //         <div class="footerDiv">
-        //             <div class="footerIbeto">
-        //                 <a href="http://www.mec.ac.in/">
-        //                     <img src="../images/Copy of mec_logo.png" />
-        //                 </a>
-        //             </div>
-        //             <div class="icon1">
-        //                 <a href="//twitter.com/excelmec?lang=en">
-        //                     <img src="../images/tweet.svg" />
-        //                 </a>
-
-        //             </div>
-        //             <div class="icon2">
-        //                 <a href="//www.facebook.com/excelmec/">
-        //                     <img src="../images/fb.svg" />
-        //                 </a>
-        //             </div>
-        //             <div class="icon3">
-        //                 <a href="//www.instagram.com/excelmec/?hl=en">
-        //                     <img src="../images/instagram (2).png" />
-        //                 </a>
-        //             </div>
-        //             <div class="footerExcel">
-        //                 <a href="//excelmec.org">
-        //                     <img src="../images/excel 2019.png" />
-        //                 </a>
-        //             </div>
-        //             <div class="Excelbrand">
-        //                 <span>Made with <i class="far fa-heart" style="color:coral;"></i> Excel 2019</span>
-        //             </div>
-        //         </div>
-        //     </footer>
-        // </div>
-    
+            <div className='col-4'>
+               <ul class='social-icons'>
+                  <li>
+                     <a href='' class='social-icon'>
+                        {" "}
+                        <i class='fa fa-facebook'></i>
+                     </a>
+                  </li>
+                  <li>
+                     <a href='' class='social-icon'>
+                        {" "}
+                        <i class='fa fa-instagram'></i>
+                     </a>
+                  </li>
+                  <li>
+                     <a href='' class='social-icon'>
+                        {" "}
+                        <i class='fa fa-linkedin'></i>
+                     </a>
+                  </li>
+               </ul>
+            </div>
+            <div className='col-4'>
+               <img src='https://ibeto.excelmec.org/assets/logo.png' alt='' width='140' />
+            </div>
+         </div>
+      </div>
+   );
 };
 
 export default Footer;
