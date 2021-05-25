ff19dddcc5f9947cf88fe026f460c0cd896ad893
artibhatia13 <bhatiaarti138@gmail.com>
2021-05-25 18:19:51 +0530

    preloading screen

--- ibeto-front-end/src/App.js
+++ ibeto-front-end/src/App.js
@@ -1,9 +1,15 @@
+import React, { useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
 import Dashboard from "./components/dashboard/Dashboard";
 import IbetoJnr from "./components/ibetoJnr/IbetoJnr";
 import IbetoMainSite from "./components/ibetoMainSite/IbetoMainSite";
 
-function App() {
+function App({ hideLoader }) {
+
+   useEffect(() => {
+      hideLoader()
+  });
+
    return (
       <div className='App'>
          <Switch>
