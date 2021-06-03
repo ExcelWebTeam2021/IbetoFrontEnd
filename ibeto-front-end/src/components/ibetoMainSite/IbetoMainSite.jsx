482af7c8e2f17c14e8619cd859d4a2ac19e178fc
annu12340 <annujolly17@gmail.com>
2021-06-04 01:22:58 +0530

    removed some unwanted code

--- ibeto-front-end/src/components/ibetoMainSite/IbetoMainSite.jsx
+++ ibeto-front-end/src/components/ibetoMainSite/IbetoMainSite.jsx
@@ -13,8 +13,11 @@ import Sponsors from "./Sponsors";
 import FAQ from "./FAQ";
 import ProblemStmts from "./ProblemStmts";
 import "../stylesheet/Cursor.css";
+
+
 const IbetoMainSite = () => {
-   document.addEventListener("mousemove", function (e) {
+      
+      document.addEventListener("mousemove", function (e) {
       console.log("MOVED", e);
       var to_append = document.getElementsByClassName("entire-container")[0];
       var all = document.getElementsByClassName("entire-container");
@@ -32,30 +35,7 @@ const IbetoMainSite = () => {
       if (document.getElementsByClassName("entire-container").length > 100) {
          document.body.removeChild(to_append);
       }
-
-      let mousePosX = 0,
-         mousePosY = 0,
-         mouseCircle = document.getElementById("mouse-circle");
-
-      document.onmousemove = (e) => {
-         mousePosX = e.pageX;
-         mousePosY = e.pageY;
-      };
-
-      let delay = 6,
-         revisedMousePosX = 0,
-         revisedMousePosY = 0;
-
-      function delayMouseFollow() {
-         requestAnimationFrame(delayMouseFollow);
-
-         revisedMousePosX += (mousePosX - revisedMousePosX) / delay;
-         revisedMousePosY += (mousePosY - revisedMousePosY) / delay;
-
-         mouseCircle.style.top = revisedMousePosY + "px";
-         mouseCircle.style.left = revisedMousePosX + "px";
-      }
-      delayMouseFollow();
+     
    });
    return (
       <div className='ibeto-mainsite'>
