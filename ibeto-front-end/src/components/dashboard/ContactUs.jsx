c9c7cf9bbe3f3106a49caa4db3045c994a7fa4bb
annu12340 <annujolly17@gmail.com>
2021-06-06 23:24:59 +0530

    dashboard and responsiveness

--- ibeto-front-end/src/components/dashboard/ContactUs.jsx
+++ ibeto-front-end/src/components/dashboard/ContactUs.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import ContactUsIcon from "../images/ContactUsIcon.svg";
-import "../stylesheet/ResponsiveStyle.css";
+import "../stylesheet/ResponsiveDashboardStylesheet.css";
 
 const ContactUs = () => {
    return (
       <div className='white-bk contactUs-row'>
-         <a className='sendmailbtn ' href='https://mail.google.com/mail/u/0/?fs=1&to=ibeto@gmail.com&su=Doubt+regarding+ibeto&body=Our+team+%3CTeamName%3E+had+a+query+regarding+...&tf=cm' target='_blank' >
+         <a className='sendmailbtn ' href='https://mail.google.com/mail/u/0/?fs=1&to=ibeto@gmail.com&su=Doubt+regarding+ibeto&body=Our+team+%3CTeamName%3E+had+a+query+regarding+...&tf=cm' target='_blank'>
             <div className='row '>
                <div className='col-8 contactUs-text'>
                   <p>Any queries</p>
                   <p style={{ color: "#f8f9fa" }}>Contact us</p>
                </div>
                <div className='col-4'>
-                  <img src={ContactUsIcon} alt='' width='60' />
+                  <img src={ContactUsIcon} className='img-fluid' alt='' width='60' />
                </div>
             </div>
          </a>
