b6213ad330e37aa946576f5f9a3b9b93ff91536d
aishh2k <aiswarya2000may@gmail.com>
2021-06-11 14:22:03 -0700

    changes

--- ibeto-front-end/src/components/ibetoJnr/LandingPage.jsx
+++ ibeto-front-end/src/components/ibetoJnr/LandingPage.jsx
@@ -25,7 +25,7 @@ const LandingPage = () => {
    return (
       <main>
          {/* <div className='animation-container darker-bk' id='Home' ref={LandingPage}></div> */}
-         <div className='animation-container darker-bk' id='Home'>
+         <div className='animation-container darker-bk' id='Junior-Home'>
             <img src={plan_landingpage} alt='' />
             <div class='custom-shape-divider-bottom-1622696646'>
                <svg data-name='Layer 1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1200 120' preserveAspectRatio='none'>
