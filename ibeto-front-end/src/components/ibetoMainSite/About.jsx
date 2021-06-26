5ac0e7d267d2147052075fdf1da237a9011b3279
farhan-najeeb <farhannajeeb77@gmail.com>
2021-06-26 21:14:39 +0530

    added blob effects to dashboard btn

--- ibeto-front-end/src/components/ibetoMainSite/About.jsx
+++ ibeto-front-end/src/components/ibetoMainSite/About.jsx
@@ -29,13 +29,43 @@ const About = () => {
                      but will also help in understanding the need for innovation in the real world.
                   </p>
                </div>
-               <div className=''>
+               {/* <div className=''>
                   <button className='btn mt-4 about-btn button-shadow'>
                      <Link to='/dashboard' className='about-btn-text'>
                         Dashboard
                      </Link>
                   </button>
-               </div>
+               </div> */}
+
+{/* <div class="buttons"> */}
+  <button class="btn mt-4 blob-btn button-shadow">
+  <Link to='/dashboard' className='about-btn-text'>
+                        Dashboard
+                     </Link>
+    <span class="blob-btn__inner">
+      <span class="blob-btn__blobs">
+        <span class="blob-btn__blob"></span>
+        <span class="blob-btn__blob"></span>
+        <span class="blob-btn__blob"></span>
+        <span class="blob-btn__blob"></span>
+      </span>
+    </span>
+  </button>
+  <br/>
+
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+  <defs>
+    <filter id="goo">
+      <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10"></feGaussianBlur>
+      <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 21 -7" result="goo"></feColorMatrix>
+      <feBlend in2="goo" in="SourceGraphic" result="mix"></feBlend>
+    </filter>
+  </defs>
+</svg>
+
+
+
+{/* </div> */}
             </div>
          </div>
       </div>
