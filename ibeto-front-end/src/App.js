23f5c2443328201241d4f6920d4f59bf91e09641
annu12340 <annujolly17@gmail.com>
2021-08-23 15:58:44 +0530

    trying out google analytics

--- ibeto-front-end/src/App.js
+++ ibeto-front-end/src/App.js
@@ -5,7 +5,7 @@ import Dashboard from "./components/dashboard/Dashboard";
 import IbetoMainSite from "./components/ibetoMainSite/IbetoMainSite";
 import ReactGA from 'react-ga';
 
-ReactGA.initialize('UA-199155692-1');
+ReactGA.initialize('UA-283779623-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App({ hideLoader }) {
